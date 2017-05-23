13f0280c34d8933d84f8c4f54644c458ea63d51b
Rivaaj Jumna <rivaaj@gmail.com>
2017-05-23 23:38:49 +0800

    Added build script for jdk-8u131 and docker files for nanoserver and core

---
+++ build.ps1
@@ -0,0 +1,2 @@
+docker build -t rivaaj/winjavabase:jdk-8u131-nanoserver -f .\nanoserver\Dockerfile .
+docker build -t rivaaj/winjavabase:jdk-8u131-windowsservercore -f .\windowsservercore\Dockerfile .
